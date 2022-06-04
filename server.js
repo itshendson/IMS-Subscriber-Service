7346ef51c3f7324cb034a2e4911c4096b5a3f94f
Hendson <hendsonszeto@live.com>
2022-06-04 10:45:50 -0700

    Add stubbed endpoints

--- server.js
+++ server.js
@@ -8,12 +8,27 @@ const app = express();
 /**
  * -----------CONFIGURE EXPRESS MIDDLEWARE-------------
  */
- app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
- app.use(cors());
- app.set('view engine', 'ejs');
- app.use(express.static('public'));
- 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+/**
+ * -----------ENDPOINTS-------------
+ */
+app.get('/ims/subscriber/:phoneNumber', () => {
+    // Retrieve the subscriber identified by the provided phone number
+})
+
+app.put('/ims/subscriber/:phoneNumber', () => {
+    // Add or update a subscriber identified by the provided phone number
+})
+
+app.delete('/ims/subscriber/:phoneNumber', () => {
+    // Remove the subscriber identified by the phone number
+})
+
 
 /**
  * -----------SERVER-------------
