f869c970261129778b0824e6df5e7f417bc89bca
Hendson <hendsonszeto@live.com>
2022-06-04 16:15:18 -0700

    Implement GET request endpoint

--- server.js
+++ server.js
@@ -15,10 +15,40 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 /**
+ * -----------DATA STORAGE-------------
+ */
+ let subscribers = [
+    {
+        "phoneNumber": "18675181010",
+        "username": "16045906403",
+        "password": "p@ssw0rd!",
+        "domain": "ims.mnc660.mcc302.3gppnetwork.org",
+        "status": "ACTIVE",
+        "features": {
+            "callForwardNoReply": {
+            "provisioned": true,
+            "destination": "tel:+18675182800"
+             }
+        }
+    }
+]
+
+/**
  * -----------ENDPOINTS-------------
  */
-app.get('/ims/subscriber/:phoneNumber', () => {
+app.get('/ims/subscriber/:phoneNumber', (req, res) => {
     // Retrieve the subscriber identified by the provided phone number
+    const phoneNumber = req.params.phoneNumber;
+
+    for (let i = 0; i < subscribers.length; i++) {
+        if (phoneNumber === subscribers[i].phoneNumber) {
+            console.log('Matched!');
+            res.status(200).send(subscribers[i]);
+        } else {
+            console.log('No match')
+            res.status(404).json({ message: "Phone number not found." })
+        }
+    }
 })
 
 app.put('/ims/subscriber/:phoneNumber', () => {
