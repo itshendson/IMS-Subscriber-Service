c6a36af87116071f10880d3c1ec6987f1362c7a0
Hendson <hendsonszeto@live.com>
2022-06-07 17:16:17 -0700

    Remove cors since it is not needed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
-const cors = require("cors");
 
 const app = express();
 
@@ -10,7 +9,6 @@ const app = express();
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(express.static("public"));
 
 /**
