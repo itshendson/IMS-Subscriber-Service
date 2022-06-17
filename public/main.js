cd1c516ece1eeac08102b6f90d35e6f03793c00f
Hendson <hendsonszeto@live.com>
2022-06-17 11:04:39 -0700

    Implement feature to fill in modal input fields

--- public/main.js
+++ public/main.js
@@ -147,10 +147,30 @@ const openModal = async (phoneNumberInput) => {
   document.getElementById("modal-overlay").style.display = "block";
   document.getElementById("modal-number-display").textContent =
     phoneNumberInput;
+
+  // Fill in modal input fields
+  const response = await fetch(`/ims/subscriber/${phoneNumberInput}`);
+  const responseJSON = await response.json();
+
+  if (response.status === 200) {
+    document.getElementById("input-username").value = responseJSON.username;
+    document.getElementById("input-password").value = "******";
+    document.getElementById("input-domain").value = responseJSON.domain;
+    document.getElementById("input-status").value = responseJSON.status;
+  } else {
+    message.textContent = "";
+  }
 };
 
 const closeModal = () => {
   document.getElementById("modal-overlay").style.display = "none";
+
+  // Reset modal input fields to blank on close
+  document.getElementById("modal-number-display").textContent = "";
+  document.getElementById("input-username").value = "";
+  document.getElementById("input-password").value = "";
+  document.getElementById("input-domain").value = "";
+  document.getElementById("input-status").value = "";
 };
 
 clearDisplay = () => {
